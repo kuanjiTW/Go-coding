58913615c95a112c25a0543a2a22c412f66348e5
KuanjiTW <ryanopop99@gmail.com>
2025-09-19 12:04:15 +0800

    switch/switch.go

--- switch/switch.go
+++ switch/switch.go
@@ -10,16 +10,29 @@ func main() {
 		Guest
 	)
 	var r = NormalUser
+	fmt.Print("Enter your role (0-3): ")
+	fmt.Scan(&r)
+	//solution A
 	switch r {
-	case Administrator:
+	case Administrator, PowerUser:
 		fmt.Println("Access allowed")
-	case PowerUser:
-		fmt.Println("Access allowed")
-	case NormalUser:
+	case NormalUser, Guest:
 		fmt.Println("Access denied")
-	case Guest:
+	default:
 		fmt.Println("Access denied")
-		// default:
-		// 	fmt.Println("Access denied")
+	}
+
+	//solution B
+	// if r == Administrator || r == PowerUser {
+	// 	fmt.Println("Access allowed")
+	// } else if r == NormalUser || r == Guest {
+	// 	fmt.Println("Access denied")
+	// } else {
+	// 	fmt.Println("Access denied")
+	// }
+
+	var amount int = 7
+	if amount < 0 {
+
 	}
 }
