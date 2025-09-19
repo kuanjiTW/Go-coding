b3e1749c595428aaeb5472aa7bbc7eeabd373dbf
KuanjiTW <ryanopop99@gmail.com>
2025-09-19 10:58:27 +0800

    logic/logic.go

--- logic/logic.go
+++ logic/logic.go
@@ -3,18 +3,24 @@ package main
 import "fmt"
 
 func main() {
-	var balance int = 1000
-	var withdraw int
-	fmt.Print("Withdraw amount: ")
-	fmt.Scan(&withdraw)
-	if withdraw < 0 {
-		fmt.Println("The amount must be greater than 0")
-	} else if withdraw >= balance {
-		//the branch
-		fmt.Println("The amount must be  smaller than or equal to balance")
+	// var balance int = 1000
+	// var withdraw int
+	// fmt.Print("Withdraw amount: ")
+	// if fmt.Scan(&withdraw); withdraw < 0 {
+	// 	fmt.Println("The amount must be greater than 0")
+	// } else if withdraw >= balance {
+	// 	//the branch
+	// 	fmt.Println("The amount must be  smaller than or equal to balance")
+	// } else {
+	// 	//else branch
+	// 	balance -= withdraw
+	// 	fmt.Println("balance:", balance)
+	// }
+	n := -4
+	if n := 7; n > 0 {
+		fmt.Println(n, "is postitive")
 	} else {
-		//else branch
-		balance -= withdraw
-		fmt.Print("balance:", balance)
+		fmt.Println(n, "is not postitive")
 	}
+	fmt.Println("n =", n)
 }
