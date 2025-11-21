9b225698e10ccc00582344d4261420382ba07607
KuanjiTW <ryanopop99@gmail.com>
2025-11-21 11:16:42 +0800

    app/act/act.go

---
+++ app/act/act.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func GetTypeName(v any) string {
+	switch v.(type) {
+	case int:
+		return "int"
+	case float64:
+		return "float64"
+	case string:
+		return "string"
+	case complex128:
+		return "complex128"
+	default:
+		return "unknown"
+	}
+}
+
+func main() {
+	s := []any{any(1), any(3.13), any("hello"), any(1 + 2i)}
+	for _, e := range s {
+		fmt.Println(e, ":", GetTypeName(e))
+	}
+}
--- app/array2/array.go
+++ app/array2/array.go
@@ -2,12 +2,50 @@ package main
 
 import "fmt"
 
-func main() {
-	//incr(5)
-	//incr(3.14)
-	//incr("Hello world")
+type Counter struct {
+	count int
+}
 
-	var x any = 5
-	y := x.(int)
-	fmt.Println(x, y)
+func incr(n any) any {
+	switch m := n.(type) {
+	case int:
+		return any(m + 1)
+	case float64:
+		return any(m + 1.0)
+	case string:
+		return any(m + "")
+	case *Counter:
+		m.count += 1
+		return any(m)
+	default:
+		fmt.Println("unsupported type")
+		return n
+	}
+
+	// 	if m, ok := n.(int); ok {
+	// 		return any(m + 1)
+	// 	} else if m, ok := n.(float64); ok {
+	// 		return any(m + 1.0)
+	// 	} else if m, ok := n.(string); ok {
+	// 		return any(m + "")
+	// 	} else if m, ok := n.(*Counter); ok {
+	// 		m.count += 1
+	// 		return any(m)
+	// 	} else {
+	// 		fmt.Println("unsupported type")
+	// 		return n
+	// 	}
+}
+
+func main() {
+	fmt.Println(incr(5))
+	fmt.Println(incr(3.14))
+	fmt.Println(incr("hello"))
+	fmt.Println(incr(&Counter{7}))
+	// var x any = 5
+	// y := x.(int)
+	// fmt.Println(x, y)
+	// if z, ok := x.(string); ok {
+	// 	fmt.Println(z)
+	// }
 }
