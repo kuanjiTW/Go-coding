83196fdac6bbe911ab1e3c8edc7e8a235d61d983
KuanjiTW <ryanopop99@gmail.com>
2025-12-05 10:58:22 +0800

    app/interface/interface.go

--- app/interface/interface.go
+++ app/interface/interface.go
@@ -6,21 +6,75 @@ type Speaker interface {
 	Speak() string
 }
 
-type cat struct{}
-
-func (c cat) Speak() string {
-	return "Meow Meow!"
+type cat struct {
+	name string
+	age  int
+}
+type dog struct{}
+type person struct {
+	name string
 }
 
-type dog struct{}
+func (c cat) Speak() string {
+	return "Meow Meow"
+}
+func (c cat) GetName() string {
+	return c.name
+}
+func (c cat) GetAge() int {
+	return c.age
+}
+func (c cat) SetName(name string) {
+	c.name = name
+}
 
 func (d dog) Speak() string {
-	return "Woof Woof!"
+	return "Woof Woof"
+}
+
+func (p person) Speak() string {
+	return "Hello, my name is " + p.name
+}
+
+type I int
+
+func (i I) Speak() string {
+	return "I am an integer"
+}
+
+func saySomething(ss ...Speaker) {
+	for _, s := range ss {
+		fmt.Println(s.Speak())
+	}
 }
 
 func main() {
+	n := 10
+	fmt.Println(I(n).Speak())
+	fmt.Println()
+
 	c := cat{}
 	d := dog{}
-	fmt.Println(c.Speak())
-	fmt.Println(d.Speak())
+	p := person{name: "Kevin"}
+	saySomething(c, d, p)
+	fmt.Println()
+
+	c = cat{"Alice", 3}
+	c.SetName("Bob")
+	fmt.Println(c.GetName())
+	// c1.Greeting = func() string {
+	// 	return "meow meow meow , meoooooooow"
+	// }
+	// fmt.Println("c1:")
+	// fmt.Println(c1.Speak())
+	// fmt.Println(c1.Greeting())
+
+	// c2 := cat{}
+	// c2.Greeting = func() string {
+	// 	return "meow~~~~~~~~~~~~~~~~"
+	// }
+	// fmt.Println("c2:")
+	// fmt.Println(c2.Speak())
+	// fmt.Println(c2.Greeting())
+
 }
